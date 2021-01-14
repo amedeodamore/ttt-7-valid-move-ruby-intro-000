b6b23bf5967b24771e66116b3d934ec0846e4e9d
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-14 01:39:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,16 +14,3 @@ end
 def position_taken?(board, index)
 board[index] != " "
 end
-
-#def valid_move?(board, index)
-  # if index.between?(1,9)
-  #   if !position_taken?(board, index)
-  #     true
-  #   end
-  # end
-#  index.between?(0,8) && !position_taken?(board, index)
-#end
-
-#def position_taken?(board, index)
-#  board[index] != " "
-#end
